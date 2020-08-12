4a14fc19ce31cc0aef3cf58e4a52826b284af7f8
Sybille Peters <sypets@gmx.de>
2020-08-12 20:32:03 +0200

    Initial version

---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Migrate to Composer',
+    'description' => 'This extension dumps information about currently loaded extensions to screen to be used as composer.json or to run composer commands.',
+    'category' => '',
+    'author' => 'Sybille Peters',
+    'author_email' => 'sypets@gmx.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '8.7.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.999'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
+
+
+
+
